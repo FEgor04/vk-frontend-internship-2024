82f87a57e63f39badad316b481f54cc3a77f3391
Egor Fedorov <fegor2004@gmail.com>
2024-03-08 00:07:45 +0300

    feat(build): add tsconfig-paths plugin

--- src/app/providers/vkui-provider.tsx
+++ src/app/providers/vkui-provider.tsx
@@ -2,20 +2,28 @@ import {
   AdaptivityProvider,
   AppRoot,
   ConfigProvider,
+  Group,
   Panel,
+  PanelHeader,
+  SimpleCell,
   View,
 } from "@vkontakte/vkui";
 
-export const VkUiProvider = () => (
-  <ConfigProvider>
-    <AdaptivityProvider>
-      <AppRoot>
-        <View activePanel="catfact">
-          <Panel id="catfact">
-            <h1>Hello there!</h1>
-          </Panel>
-        </View>
-      </AppRoot>
-    </AdaptivityProvider>
-  </ConfigProvider>
-);
+export const VkUiProvider = () => {
+  return (
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <AppRoot>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>Catfact</PanelHeader>
+              <Group>
+                <SimpleCell>Hello</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
+  );
+};

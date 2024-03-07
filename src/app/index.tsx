6523b718a3fee718bb7f64ca695bca7f959f5597
Egor Fedorov <fegor2004@gmail.com>
2024-03-07 23:20:53 +0300

    feat: set up fsd methodology
    - revert typescript to 5.3.3 for eslint compability
    - install typescript-eslint-resolver for import-order rule

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './global.css'
+
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1>Hello FSD</h1>
   </React.StrictMode>,
 )

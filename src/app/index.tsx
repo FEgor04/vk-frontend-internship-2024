075cb8f0801b4fd27ad22de02e55475e23f62e2f
Egor Fedorov <fegor2004@gmail.com>
2024-03-07 23:24:42 +0300

    fix(index.html): fix script name

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,6 @@ import './global.css'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello FSD</h1>
+    <h1>Hello FSD</h1>
   </React.StrictMode>,
 )

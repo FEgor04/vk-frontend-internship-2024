8d0dbb97f2248030dfceaf64c434de5160c3aec8
Egor Fedorov <fegor2004@gmail.com>
2024-03-08 09:49:23 +0300

    feat: load catfact on button click

--- src/pages/catfact/index.tsx
+++ src/pages/catfact/index.tsx
@@ -1,16 +1,56 @@
-import { Button, ButtonGroup, Group, PanelHeader } from "@vkontakte/vkui";
+import { getCatfactQueryOptions } from "@/app/entities/catfact";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Button,
+  ButtonGroup,
+  Group,
+  PanelHeader,
+  Textarea,
+} from "@vkontakte/vkui";
+import { useRef } from "react";
 
 type Props = {
   goToAgify: () => void;
 };
 
 export function CatfactPanel({ goToAgify }: Props) {
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const { isLoading, refetch } = useQuery({
+    ...getCatfactQueryOptions(),
+    enabled: false,
+  });
+
+  function onLoadCatfact() {
+    void refetch().then((data) => {
+      if (data.isSuccess) {
+        if (ref.current) {
+          const catfact = data.data;
+          const firstSpacePosition = catfact.fact.indexOf(" ");
+          ref.current.value = catfact.fact;
+          ref.current.focus();
+          if (firstSpacePosition != -1) {
+            ref.current.setSelectionRange(
+              firstSpacePosition,
+              firstSpacePosition,
+            );
+          }
+        }
+      }
+    });
+  }
+
   return (
     <>
       <PanelHeader>Catfact</PanelHeader>
       <Group>
+        <Textarea
+          getRef={ref}
+          placeholder="Нажми на кнопку - получишь котофакт"
+        />
         <ButtonGroup>
-          <Button>Загрузить котофакт</Button>
+          <Button onClick={onLoadCatfact} loading={isLoading}>
+            Загрузить котофакт
+          </Button>
           <Button appearance="neutral" onClick={goToAgify}>
             Перейти к форме
           </Button>

f0396c5749d4ba8c721f7f5465f8926aa9da780e
Egor Fedorov <fegor2004@gmail.com>
2024-03-08 11:58:30 +0300

    feat: add catfact description

--- src/pages/catfact/index.tsx
+++ src/pages/catfact/index.tsx
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import {
+  Accordion,
   Button,
   ButtonGroup,
+  Div,
   Group,
   PanelHeader,
+  Spacing,
   Textarea,
 } from "@vkontakte/vkui";
 import { useRef } from "react";
 import { getCatfactQueryOptions } from "@/entities/catfact";
+import { AccordionContent } from "@vkontakte/vkui/dist/components/Accordion/AccordionContent";
 
 type Props = {
   goToAgify: () => void;
@@ -39,6 +43,8 @@ export function CatfactPanel({ goToAgify }: Props) {
     });
   }
 
+  const infoStyle = { color: "var(--vkui--color_text_subhead)" };
+
   return (
     <>
       <PanelHeader>Catfact</PanelHeader>
@@ -55,6 +61,44 @@ export function CatfactPanel({ goToAgify }: Props) {
             К гадалке
           </Button>
         </ButtonGroup>
+        <Spacing size={16} />
+        <Accordion>
+          <Accordion.Summary>Кэшируются ли запросы?</Accordion.Summary>
+          <Accordion.Content>
+            <Div style={infoStyle}>
+              Конечно нет! При каждом клике отправляется вызывается метод{" "}
+              <code>refetch</code> и за котофактом отправляется новый запрос
+            </Div>
+          </Accordion.Content>
+        </Accordion>
+        <Spacing size={8} />
+        <Accordion>
+          <Accordion.Summary>
+            Каким образом выставляется курсор?
+          </Accordion.Summary>
+          <Accordion.Content>
+            <Div style={infoStyle}>
+              Курсор выставялется за счет хранения ссылки на компонент{" "}
+              <code>Textarea</code>, с помощью метода{" "}
+              <code>setSelectionRange()</code>
+            </Div>
+          </Accordion.Content>
+        </Accordion>
+        <Spacing size={8} />
+        <Accordion>
+          <Accordion.Summary>Что с декомпозицией?</Accordion.Summary>
+          <Accordion.Content>
+            <Div style={infoStyle}>
+              Декомпозиция крайне простая: тип <code>Catfact</code> и метод{" "}
+              <code>getCatfactQueryOptions</code> хранятся в слайсе{" "}
+              <code>catfact</code> слоя <code>entities</code>. Разметка данной
+              страницы полностью находится в слайсе <code>catfact</code> в{" "}
+              <code>pages</code>. Возможно при дальнейшем рефакторинге я вынесу
+              часть логики в <code>feature</code> <code>get-catfact</code>,
+              однако это кажется очень большим переусложнением.
+            </Div>
+          </Accordion.Content>
+        </Accordion>
       </Group>
     </>
   );

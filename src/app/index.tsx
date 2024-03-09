88435bc2f2dfa8b65414478804f191e8f4dc8a03
Egor Fedorov <fegor2004@gmail.com>
2024-03-09 15:49:15 +0300

    feat: add vk-bridge package

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
+import bridge from "@vkontakte/vk-bridge";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@vkontakte/vkui/dist/vkui.css";
 
 import { Providers } from "./providers";
+
+void bridge.send("VKWebAppInit");
 
 const queryClient = new QueryClient({
   defaultOptions: {
